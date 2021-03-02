1c70fe5fbfc789e25fe0bddfef88cfc0ffb94d09
Alfonso Lopez <alfonso_lopezjimenez@hotmail.com>
2021-03-02 20:52:34 +0100

    Ejercicios PyQt5
    Signed-off-by: Alfonso Lopez <alfonso_lopezjimenez@hotmail.com>

---
+++ 5 - Python con BBDD/Prueba/grafica.py
@@ -0,0 +1,9 @@
+import matplotlib.pyplot as plot
+
+periodos = ['2017', '2018', '2019', '2020', '2021']
+ganancias = [80, 70, 45, 90, 10]
+
+plot.bar(periodos, ganancias)
+plot.title('Ganancia en años')
+plot.plot(100)
+plot.show()
